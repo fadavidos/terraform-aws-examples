a7be6801c636a1eb0417d78a8efa07d98d640a8c
Fabian Osorio <fabianosorio@seven4n.com>
2019-07-10 20:55:30 -0500

    Update to terraform 12

--- example1/main.tf
+++ example1/main.tf
@@ -1,118 +1,119 @@
 provider "aws" {
-	region 		= "${var.in_region}"
-	profile 	= "${var.in_profile}"
+  region  = var.in_region
+  profile = var.in_profile
 }
 
-data "aws_availability_zones" "all" {}
+data "aws_availability_zones" "all" {
+}
 
 variable "in_server_port" {
-	description = "The port the server will use for HTTP request"
-	default 		= 8080
+  description = "The port the server will use for HTTP request"
+  default     = 8080
 }
 
 variable "in_profile" {
-	description = "The name awscli's profile to use"
-	type 				= "string"
+  description = "The name awscli's profile to use"
+  type        = string
 }
 
 variable "in_region" {
-	description 	= "Region's name where going to deploy"
-	default 			= "us-east-1"
-	type 					= "string"
+  description = "Region's name where going to deploy"
+  default     = "us-east-1"
+  type        = string
 }
 
 variable "name_instance_launch" {
-	description = "The instances name of launch configure"
-	default 		=	"Terraform-asg-axample" 
+  description = "The instances name of launch configure"
+  default     = "Terraform-asg-axample"
 }
 
-
 resource "aws_launch_configuration" "my_launch_example" {
-	image_id 					= "ami-40d28157"
-	instance_type 		= "t2.micro"
-	security_groups 	= ["${aws_security_group.sg_instances.id}"]
+  image_id        = "ami-40d28157"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.sg_instances.id]
 
-	user_data 				=  <<-EOF
+  user_data = <<-EOF
 	#!/bin/bash
 	echo "Hello world, from EC2" > index.html
 	nohup busybox httpd -ff -p "${var.in_server_port}" &
-	EOF
+EOF
 
-	lifecycle {
-		create_before_destroy =  true
-	}
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_autoscaling_group" "my_auto_scaling" {
-	launch_configuration 	= "${aws_launch_configuration.my_launch_example.id}"
-	availability_zones 		= ["${data.aws_availability_zones.all.names}"]
+  launch_configuration = aws_launch_configuration.my_launch_example.id
+  availability_zones = data.aws_availability_zones.all.names
 
-	load_balancers 				= ["${aws_elb.my_elb_asg.name}"]
-	health_check_type	 		= "ELB"
+  load_balancers = [aws_elb.my_elb_asg.name]
+  health_check_type = "ELB"
 
-	min_size = 3
-	max_size = 3
+  min_size = 3
+  max_size = 3
 
-	tag {
-		key									= "Name"
-		value 							= "${var.name_instance_launch}"
-		propagate_at_launch	= true
-	}
+  tag {
+    key = "Name"
+    value = var.name_instance_launch
+    propagate_at_launch = true
+  }
 }
 
 resource "aws_security_group" "sg_instances" {
-	name 			= "sgexamples"
-	ingress {
-		from_port 	= "${var.in_server_port}"
-		to_port 		= "${var.in_server_port}"
-		protocol		= "tcp"
-		cidr_blocks	=["0.0.0.0/0"]
-	}
+  name = "sgexamples"
+  ingress {
+    from_port = var.in_server_port
+    to_port = var.in_server_port
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-	lifecycle {
-		create_before_destroy = true
-	}
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_elb" "my_elb_asg" {
-	name = "myElb"
-	availability_zones 	= ["${data.aws_availability_zones.all.names}"]
-	security_groups 		= ["${aws_security_group.sg_elb.id}"]
+  name = "myElb"
+  availability_zones = data.aws_availability_zones.all.names
+  security_groups = [aws_security_group.sg_elb.id]
 
-	listener {
-		lb_port 					= 80
-		lb_protocol 			= "http"
-		instance_port 		= "${var.in_server_port}"
-		instance_protocol	= "http"
-	}
+  listener {
+    lb_port = 80
+    lb_protocol = "http"
+    instance_port = var.in_server_port
+    instance_protocol = "http"
+  }
 
-	health_check {
-		healthy_threshold 		= 2
-		unhealthy_threshold 	= 2
-		timeout								= 3
-		interval							= 30
-		target								= "HTTP:${var.in_server_port}/"
-	}
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    interval = 30
+    target = "HTTP:${var.in_server_port}/"
+  }
 }
 
 resource "aws_security_group" "sg_elb" {
-	name 				= "sgElb"
-	ingress {
-		from_port			= 80
-		to_port				= 80
-		protocol 			= "tcp"
-		cidr_blocks 	= ["0.0.0.0/0"]
-	}
+  name = "sgElb"
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-	egress {
-		from_port		= 0
-		to_port			= 0
-		protocol 		= "-1"
-		cidr_blocks	= ["0.0.0.0/0"]
-	}
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
-
 
 output "out_dns_elb" {
-	value = "${aws_elb.my_elb_asg.dns_name}"
+  value = aws_elb.my_elb_asg.dns_name
 }
+
