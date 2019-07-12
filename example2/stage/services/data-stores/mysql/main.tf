958ed1174e72089221a1cbed7bfd883cf1d9fc00
Fabian Osorio <fabianosorio@seven4n.com>
2019-07-12 00:09:54 -0500

    remote state is available for version 12 of terraform

--- example2/stage/services/data-stores/mysql/main.tf
+++ example2/stage/services/data-stores/mysql/main.tf
@@ -5,18 +5,13 @@ provider "aws" {
 resource "aws_db_instance" "ex2_example" {
   engine              = "mysql"
   allocated_storage   = 10
-  instance_class     = "db.t2.micro"
+  instance_class      = "db.t2.micro"
   name                = "ex2_example_database"
-  username            =  "admin"
-  password            = "${var.db_password}"
+  username            = "admin"
+  password            = var.db_password
   skip_final_snapshot = "true"
 }
 
 terraform {
-  backend "s3" {
-    bucket    = "s3-status"
-    key       = "example2/stage/services/data-stores/mysql/terraform.tfstate"
-    region    = "us-east-1"
-    encrypt   = true
-  }
+  backend "s3" {}
 }
--- example2/stage/services/data-stores/mysql/outputs.tf
+++ example2/stage/services/data-stores/mysql/outputs.tf
@@ -1,7 +1,8 @@
 output "address" {
-  value = "${aws_db_instance.ex2_example.address}"
+  value = aws_db_instance.ex2_example.address
 }
 
 output "port" {
-  value = "${aws_db_instance.ex2_example.port}"
+  value = aws_db_instance.ex2_example.port
 }
+
